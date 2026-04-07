272e7c1f434421fb94c17848c63a0c1f28e90b1f
feraxhp <feraxhp+gh@gmail.com>
2026-04-07 10:44:28 -0500

    feat(keydown): better conditioning

--- app/keymap/keydown.ts
+++ app/keymap/keydown.ts
@@ -4,20 +4,27 @@ import type { FractosView } from "fractos"
 import type { Context } from "./handler"
 import { taskTag, type FractosTaskElement } from "../components/web/task"
 
+const keymap = {
+  context: '',
+  bindings: {
+    '': () => { }
+  }
+}
+
 export const keyboardHandler = createKeybindingsHandler<[FractosView, Context]>({
   'ArrowUp': (e, v, c) => {
-    if ('task' in c) {
-      if (c.task!.isEditing) return
-      c.task!.focusGerarquicalPreviousTask()
+    if (c.task) {
+      if (c.task.isEditing) return
+      c.task.focusGerarquicalPreviousTask()
       return
     }
     
     (Array.from(document.getElementsByTagName(taskTag)).pop() as FractosTaskElement).focus()
   },
   'ArrowDown': (e, v, c) => {
-    if ('task' in c) {
-      if (c.task!.isEditing) return
-      c.task!.focusGerarquicalNextTask()
+    if (c.task) {
+      if (c.task.isEditing) return
+      c.task.focusGerarquicalNextTask()
       return
     }
     
